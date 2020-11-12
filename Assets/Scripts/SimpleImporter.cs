4bf7ea986d8053890043b6fbacac841423e1134d
ruanyi <iynaur87@sina.com>
2020-11-12 14:44:03 +0800

    change shader to show point color

--- Assets/Scripts/SimpleImporter.cs
+++ Assets/Scripts/SimpleImporter.cs
@@ -21,27 +21,7 @@ using Newtonsoft.Json;
 
 namespace PointCloudExporter
 {
-	public class JsonTools
-	{
-		// 从一个对象信息生成Json串
-		public static string ObjectToJson(object obj)
-		{
-			DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
-			MemoryStream stream = new MemoryStream();
-			serializer.WriteObject(stream, obj);
-			byte[] dataBytes = new byte[stream.Length];
-			stream.Position = 0;
-			stream.Read(dataBytes, 0, (int)stream.Length);
-			return Encoding.UTF8.GetString(dataBytes);
-		}
-		// 从一个Json串生成对象信息
-		public static object JsonToObject(string jsonString, object obj)
-		{
-			DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
-			MemoryStream mStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
-			return serializer.ReadObject(mStream);
-		}
-	}
+	
 	public class SimpleImporter
 	{
 		// Singleton
@@ -111,7 +91,10 @@ namespace PointCloudExporter
 				float y = Convert.ToSingle(jp["y"]);
 				float z = Convert.ToSingle(jp["z"]);
 				data.vertices[i] = new Vector3(x, y, z);
-
+				float r = Convert.ToSingle(jp["r"])/255;
+				float g = Convert.ToSingle(jp["g"])/255;
+				float b = Convert.ToSingle(jp["b"])/255;
+				data.colors[i] = new Color(r, g, b);
 			}
 			Debug.Log("after MeshInfos.txt");
 			return data;
